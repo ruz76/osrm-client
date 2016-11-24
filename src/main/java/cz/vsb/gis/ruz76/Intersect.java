c1d314d6f1cb146c256e25a300dde19b2c16b31b
ruz76 <jan.ruzicka.vsb@gmail.com>
2016-11-24 14:49:15 +0100

    In memory Feature Collection

--- src/main/java/cz/vsb/gis/ruz76/Intersect.java
+++ src/main/java/cz/vsb/gis/ruz76/Intersect.java
@@ -2,8 +2,10 @@ package cz.vsb.gis.ruz76;
 
 import com.vividsolutions.jts.geom.*;
 import com.vividsolutions.jts.geom.Point;
+import org.geotools.data.DataUtilities;
 import org.geotools.data.FileDataStore;
 import org.geotools.data.FileDataStoreFinder;
+import org.geotools.data.simple.SimpleFeatureCollection;
 import org.geotools.data.simple.SimpleFeatureIterator;
 import org.geotools.data.simple.SimpleFeatureSource;
 import org.geotools.factory.CommonFactoryFinder;
@@ -22,12 +24,12 @@ import java.util.List;
  * Created by ruz76 on 24.11.2016.
  */
 public class Intersect {
-    private final SimpleFeatureSource zsj_fs;
-	private final FileDataStore zsj_store;
+	private final SimpleFeatureCollection zsj_fc;
     public static Point start;
     public Intersect() throws IOException {
-        zsj_store = FileDataStoreFinder.getDataStore(new File("zsj.shp"));
-        zsj_fs = zsj_store.getFeatureSource();
+        FileDataStore zsj_store = FileDataStoreFinder.getDataStore(new File("zsj.shp"));
+        SimpleFeatureSource zsj_fs = zsj_store.getFeatureSource();
+        zsj_fc = DataUtilities.collection(zsj_fs.getFeatures());
     }
 	
 	public void close() throws Exception {
@@ -43,8 +45,8 @@ public class Intersect {
         start = route.getStartPoint();
         FilterFactory2 ff = CommonFactoryFinder.getFilterFactory2();
         Filter filter = ff.intersects(ff.property("the_geom"), ff.literal(route));
-        SimpleFeatureIterator zsj_fs_sfi = zsj_fs.getFeatures(filter).features();
-        System.out.println("Route id: " + routeid);
+        SimpleFeatureIterator zsj_fs_sfi = zsj_fc.subCollection(filter).features();
+        //System.out.println("Route id: " + routeid);
         List<SimpleFeature> zsjs = new ArrayList();
         while (zsj_fs_sfi.hasNext()) {
             SimpleFeature zsj = zsj_fs_sfi.next();
@@ -65,7 +67,7 @@ public class Intersect {
     * */
     public String getRoute_Zsjs_MorePrecise(LineString route, String routeid) throws Exception {
 		String zsjs_string = "";
-        System.out.println("Route id: " + routeid);
+        //System.out.println("Route id: " + routeid);
         GeometryFactory gf = new GeometryFactory();
         Coordinate cs[] = new Coordinate[2];
         List<SimpleFeature> zsjs = new ArrayList();
@@ -75,7 +77,7 @@ public class Intersect {
             LineString segment = gf.createLineString(cs);
             FilterFactory2 ff = CommonFactoryFinder.getFilterFactory2();
             Filter filter = ff.intersects(ff.property("the_geom"), ff.literal(segment));
-            SimpleFeatureIterator zsj_fs_sfi = zsj_fs.getFeatures(filter).features();
+            SimpleFeatureIterator zsj_fs_sfi = zsj_fc.subCollection(filter).features();
             double segment_lenght = 0;
             String zsjid = "";
             while (zsj_fs_sfi.hasNext()) {
