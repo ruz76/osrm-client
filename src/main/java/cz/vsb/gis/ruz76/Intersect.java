494de28d1f4b47728d2e3f0aa309197c4fbfcfee
Jan Ruzicka <jan.ruzicka.1@plus4u.net>
2020-05-16 08:22:01 +0200

    Polishing

--- src/main/java/cz/vsb/gis/ruz76/Intersect.java
+++ src/main/java/cz/vsb/gis/ruz76/Intersect.java
@@ -24,87 +24,90 @@ import java.util.List;
  * Created by ruz76 on 24.11.2016.
  */
 public class Intersect {
-	private final SimpleFeatureCollection zsj_fc;
-    public static Point start;
-    public Intersect() throws IOException {
-        FileDataStore zsj_store = FileDataStoreFinder.getDataStore(new File("zsj.shp"));
-        SimpleFeatureSource zsj_fs = zsj_store.getFeatureSource();
-        zsj_fc = DataUtilities.collection(zsj_fs.getFeatures());
-    }
-	
-	public void close() throws Exception {
-		//zsj_store.dispose();
-	}
 
-    /*
-    * Prints zsj that intersects with routes ordered based on route
-    * This is the simplest and hopefully fastest way based only on distance from start point
-    * */
-    public String getRoute_Zsjs(LineString route, String routeid) throws Exception {
-		String zsjs_string = "";
-        start = route.getStartPoint();
-        FilterFactory2 ff = CommonFactoryFinder.getFilterFactory2();
-        Filter filter = ff.intersects(ff.property("the_geom"), ff.literal(route));
-        SimpleFeatureIterator zsj_fs_sfi = zsj_fc.subCollection(filter).features();
-        //System.out.println("Route id: " + routeid);
-        List<SimpleFeature> zsjs = new ArrayList();
-        while (zsj_fs_sfi.hasNext()) {
-            SimpleFeature zsj = zsj_fs_sfi.next();
-            zsjs.add(zsj);
-        }
-        Collections.sort(zsjs, new CentroidDistanceComparator());
-		for (int i = 0; i < zsjs.size(); i++) {
-			SimpleFeature zsj = (SimpleFeature) zsjs.get(i);
-			zsjs_string += "INSERT INTO zsjs (zsjid_from_to, zsjid, seq) VALUES ('" + routeid + "', '" + zsj.getAttribute("zsjid") + "', " + i + ");\n";
-		}
-		return zsjs_string;
-        
-    }
+  private final SimpleFeatureCollection zsj_fc;
+  public static Point start;
 
-    /*
-    * Prints zsj that intersects with routes ordered based on route
-    * This is the more complicated way and slower way based on comparison of each segment in linestring
-    * */
-    public String getRoute_Zsjs_MorePrecise(LineString route, String routeid) throws Exception {
-		String zsjs_string = "";
-        //System.out.println("Route id: " + routeid);
-        GeometryFactory gf = new GeometryFactory();
-        Coordinate cs[] = new Coordinate[2];
-        List<SimpleFeature> zsjs = new ArrayList();
-        for (int i = 0; i < route.getNumPoints() - 1; i++) {
-            cs[0] = route.getPointN(i).getCoordinate();
-            cs[1] = route.getPointN(i+1).getCoordinate();
-            LineString segment = gf.createLineString(cs);
-            FilterFactory2 ff = CommonFactoryFinder.getFilterFactory2();
-            Filter filter = ff.intersects(ff.property("the_geom"), ff.literal(segment));
-            SimpleFeatureIterator zsj_fs_sfi = zsj_fc.subCollection(filter).features();
-            double segment_lenght = 0;
-            String zsjid = "";
-            while (zsj_fs_sfi.hasNext()) {
-                SimpleFeature zsj = zsj_fs_sfi.next();
-                MultiPolygon zsj_polygon = (MultiPolygon) zsj.getDefaultGeometry();
-                double cur_segment_lenght = 0;
-                try {
-                    cur_segment_lenght = zsj_polygon.intersection(segment).getLength();
-                } catch (Exception ex) {
-                    zsjs_string += "--WRONG GEOMETRY: " + ex.getMessage() + "\n";
-                }
-                if (cur_segment_lenght > segment_lenght) {
-                    segment_lenght = cur_segment_lenght;
-                    zsjid = zsj.getAttribute("zsjid").toString();
-                }
-            }
-			zsjs_string += "INSERT INTO zsjs_moreprecise (zsjid_from_to, zsjid, seq) VALUES ('" + routeid + "', '" + zsjid + "', " + i + ");\n";
-        }
-		return zsjs_string;
+  public Intersect() throws IOException {
+    FileDataStore zsj_store = FileDataStoreFinder.getDataStore(new File("zsj.shp"));
+    SimpleFeatureSource zsj_fs = zsj_store.getFeatureSource();
+    zsj_fc = DataUtilities.collection(zsj_fs.getFeatures());
+  }
+
+  public void close() throws Exception {
+    //zsj_store.dispose();
+  }
+
+  /*
+   * Prints zsj that intersects with routes ordered based on route
+   * This is the simplest and hopefully fastest way based only on distance from start point
+   * */
+  public String getRoute_Zsjs(LineString route, String routeid) throws Exception {
+    String zsjs_string = "";
+    start = route.getStartPoint();
+    FilterFactory2 ff = CommonFactoryFinder.getFilterFactory2();
+    Filter filter = ff.intersects(ff.property("the_geom"), ff.literal(route));
+    SimpleFeatureIterator zsj_fs_sfi = zsj_fc.subCollection(filter).features();
+    //System.out.println("Route id: " + routeid);
+    List<SimpleFeature> zsjs = new ArrayList();
+    while (zsj_fs_sfi.hasNext()) {
+      SimpleFeature zsj = zsj_fs_sfi.next();
+      zsjs.add(zsj);
     }
+    Collections.sort(zsjs, new CentroidDistanceComparator());
+    for (int i = 0; i < zsjs.size(); i++) {
+      SimpleFeature zsj = (SimpleFeature) zsjs.get(i);
+      zsjs_string += "INSERT INTO zsjs (zsjid_from_to, zsjid, seq) VALUES ('" + routeid + "', '" + zsj.getAttribute("zsjid") + "', " + i + ");\n";
+    }
+    return zsjs_string;
+
+  }
+
+  /*
+   * Prints zsj that intersects with routes ordered based on route
+   * This is the more complicated way and slower way based on comparison of each segment in linestring
+   * */
+  public String getRoute_Zsjs_MorePrecise(LineString route, String routeid) throws Exception {
+    String zsjs_string = "";
+    //System.out.println("Route id: " + routeid);
+    GeometryFactory gf = new GeometryFactory();
+    Coordinate cs[] = new Coordinate[2];
+    List<SimpleFeature> zsjs = new ArrayList();
+    for (int i = 0; i < route.getNumPoints() - 1; i++) {
+      cs[0] = route.getPointN(i).getCoordinate();
+      cs[1] = route.getPointN(i + 1).getCoordinate();
+      LineString segment = gf.createLineString(cs);
+      FilterFactory2 ff = CommonFactoryFinder.getFilterFactory2();
+      Filter filter = ff.intersects(ff.property("the_geom"), ff.literal(segment));
+      SimpleFeatureIterator zsj_fs_sfi = zsj_fc.subCollection(filter).features();
+      double segment_lenght = 0;
+      String zsjid = "";
+      while (zsj_fs_sfi.hasNext()) {
+        SimpleFeature zsj = zsj_fs_sfi.next();
+        MultiPolygon zsj_polygon = (MultiPolygon) zsj.getDefaultGeometry();
+        double cur_segment_lenght = 0;
+        try {
+          cur_segment_lenght = zsj_polygon.intersection(segment).getLength();
+        } catch (Exception ex) {
+          zsjs_string += "--WRONG GEOMETRY: " + ex.getMessage() + "\n";
+        }
+        if (cur_segment_lenght > segment_lenght) {
+          segment_lenght = cur_segment_lenght;
+          zsjid = zsj.getAttribute("zsjid").toString();
+        }
+      }
+      zsjs_string += "INSERT INTO zsjs_moreprecise (zsjid_from_to, zsjid, seq) VALUES ('" + routeid + "', '" + zsjid + "', " + i + ");\n";
+    }
+    return zsjs_string;
+  }
 }
 
 class CentroidDistanceComparator implements Comparator<SimpleFeature> {
-    @Override
-    public int compare(SimpleFeature a, SimpleFeature b) {
-        MultiPolygon pa = (MultiPolygon) a.getDefaultGeometry();
-        MultiPolygon pb = (MultiPolygon) b.getDefaultGeometry();
-        return pa.distance(Intersect.start) < pb.distance(Intersect.start) ? -1 : pa.distance(Intersect.start) == pb.distance(Intersect.start) ? 0 : 1;
-    }
+
+  @Override
+  public int compare(SimpleFeature a, SimpleFeature b) {
+    MultiPolygon pa = (MultiPolygon) a.getDefaultGeometry();
+    MultiPolygon pb = (MultiPolygon) b.getDefaultGeometry();
+    return pa.distance(Intersect.start) < pb.distance(Intersect.start) ? -1 : pa.distance(Intersect.start) == pb.distance(Intersect.start) ? 0 : 1;
+  }
 }
--- src/main/java/cz/vsb/gis/ruz76/Main.java
+++ src/main/java/cz/vsb/gis/ruz76/Main.java
@@ -14,43 +14,44 @@ import java.util.Map;
  * Created by jencek on 12.11.16.
  */
 public class Main {
-    /*
-    * Calculates routes between basic settlement units using OSRM.
-    * Output is written to PostGIS dump for COPY.
-    * */
-    public static void main(String args[]) throws Exception {
-        System.out.println("Test");
-		
-        ZsjDAO zsjdao = new ZsjDAO("jdbc:postgresql://localhost:5432/data", "user", "pwd");
-		
-		Map<Integer, Point> zsj = zsjdao.loadKeyValues();
-		Intersect zsjintersect = new Intersect();
-             
-		File file = new File("output.dump");
-        FileWriter fw = new FileWriter(file.getAbsoluteFile());
-        BufferedWriter bw = new BufferedWriter(fw);
-		
-		File filezsjs = new File("zsjs.sql");
-        FileWriter fwzsjs = new FileWriter(filezsjs.getAbsoluteFile());
-        BufferedWriter bwzsjs = new BufferedWriter(fwzsjs);
-		
-		File filezsjs_moreprecise = new File("zsjs_moreprecise.sql");
-        FileWriter fwzsjs_moreprecise = new FileWriter(filezsjs_moreprecise.getAbsoluteFile());
-        BufferedWriter bwzsjs_moreprecise = new BufferedWriter(fwzsjs_moreprecise);
 
-        for (Map.Entry<Integer, Point> entry : zsj.entrySet()) {
-            for (Map.Entry<Integer, Point> entry2 : zsj.entrySet()) {
-                if (!entry.getKey().equals(entry2.getKey())) {
-                    String route = Route.getRoute(entry.getValue(), entry2.getValue());
-					bwzsjs.write(zsjintersect.getRoute_Zsjs(PolylineDecoder.decodeToLineString(route), entry.getKey() + "_" + entry2.getKey()) + "\n");
-					bwzsjs_moreprecise.write(zsjintersect.getRoute_Zsjs_MorePrecise(PolylineDecoder.decodeToLineString(route), entry.getKey() + "_" + entry2.getKey()) + "\n");
-                    bw.write(entry.getKey() + "_" + entry2.getKey() + "\t" + PolylineDecoder.decodeToHex(route) + "\n");
-                }
-            }
+  /*
+   * Calculates routes between basic settlement units using OSRM.
+   * Output is written to PostGIS dump for COPY.
+   * */
+  public static void main(String args[]) throws Exception {
+    System.out.println("Test");
+
+    ZsjDAO zsjdao = new ZsjDAO("jdbc:postgresql://localhost:5432/data", "user", "pwd");
+
+    Map<Integer, Point> zsj = zsjdao.loadKeyValues();
+    Intersect zsjintersect = new Intersect();
+
+    File file = new File("output.dump");
+    FileWriter fw = new FileWriter(file.getAbsoluteFile());
+    BufferedWriter bw = new BufferedWriter(fw);
+
+    File filezsjs = new File("zsjs.sql");
+    FileWriter fwzsjs = new FileWriter(filezsjs.getAbsoluteFile());
+    BufferedWriter bwzsjs = new BufferedWriter(fwzsjs);
+
+    File filezsjs_moreprecise = new File("zsjs_moreprecise.sql");
+    FileWriter fwzsjs_moreprecise = new FileWriter(filezsjs_moreprecise.getAbsoluteFile());
+    BufferedWriter bwzsjs_moreprecise = new BufferedWriter(fwzsjs_moreprecise);
+
+    for (Map.Entry<Integer, Point> entry : zsj.entrySet()) {
+      for (Map.Entry<Integer, Point> entry2 : zsj.entrySet()) {
+        if (!entry.getKey().equals(entry2.getKey())) {
+          String route = Route.getRoute(entry.getValue(), entry2.getValue());
+          bwzsjs.write(zsjintersect.getRoute_Zsjs(PolylineDecoder.decodeToLineString(route), entry.getKey() + "_" + entry2.getKey()) + "\n");
+          bwzsjs_moreprecise.write(zsjintersect.getRoute_Zsjs_MorePrecise(PolylineDecoder.decodeToLineString(route), entry.getKey() + "_" + entry2.getKey()) + "\n");
+          bw.write(entry.getKey() + "_" + entry2.getKey() + "\t" + PolylineDecoder.decodeToHex(route) + "\n");
         }
-		bwzsjs_moreprecise.close();
-		bwzsjs.close();
-        bw.close();
-		zsjintersect.close();
+      }
     }
+    bwzsjs_moreprecise.close();
+    bwzsjs.close();
+    bw.close();
+    zsjintersect.close();
+  }
 }
